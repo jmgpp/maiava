0f84631114108f6974076ac6c81af3d28a0679ec
jmgpp <jmganancias@gmail.com>
2023-09-26 23:56:39 -0300

    Most checkout complete

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template, request
 import json
+import locale
+import sys
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    
     products = []
     with open('static/products.json', 'r') as file:
         data = json.load(file)
         products = data["products"]
 
-
+    for p in products:
+        p['priceStr'] = format_currency(p['price'])
+        
 
     return render_template('index.html', products=products)
 
 @app.route("/checkout", methods=["GET", "POST"])
 def checkout():
-    if request.method == "GET":
-        return render_template('checkout.html')
+    if request.method == "POST":
+        data = request.form.get("products")
+        items = json.loads(data)
+        
+        return render_template('checkout.html', items=items)
+        
+def format_currency(amount):
+    locale.setlocale(locale.LC_ALL, '')
+
+    num = locale.format_string("%.2f", amount, grouping=True)
+    return f"$ {num}"
 
 if __name__ == '__main__':
     app.debug = True

f473deedf410e56710eb6d09e366022c47c6bac9
jmgpp <jmganancias@gmail.com>
2023-09-27 14:36:28 -0300

    We've got a working DB!

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 from flask import Flask, render_template, request
 import json
 import locale
+import psycopg2
 
 
 app = Flask(__name__)
+
+POSTGRESQL_URI = "postgres://crtslvzw:DHUqbbQyZZ95UjYfiuInf1ERhb9ROiXn@motty.db.elephantsql.com/crtslvzw"
+connection = psycopg2.connect(POSTGRESQL_URI)
 
 @app.route('/')
 def index():
     
     products = []
-    with open('static/products.json', 'r') as file:
+    """ with open('static/products.json', 'r') as file:
         data = json.load(file)
-        products = data["products"]
+        products = data["products"] """
+
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT * FROM "public"."products" LIMIT 100""")
+            # Fetch all rows as a list of tuples
+            rows = cursor.fetchall()
+
+            # Get column names from cursor description
+            columns = [desc[0] for desc in cursor.description]
+
+            # Convert each row into a dictionary
+            for row in rows:
+                product_dict = dict(zip(columns, row))
+                products.append(product_dict)
+            
+       
 
     for p in products:
         p['priceStr'] = format_currency(p['price'])
